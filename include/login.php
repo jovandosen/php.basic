8daaa72c1b279f1c99bb8f651baf01a30f9ff871
jovandosen <jovandosen1994@gmail.com>
2019-10-31 14:45:08 +0100

    Auth logic updated

--- include/login.php
+++ include/login.php
@@ -4,7 +4,9 @@
 		<title>Login</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" type="image/png" href="/../assets/images/favicon.jpg">
 		<link rel="stylesheet" type="text/css" href="/../assets/css/app.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	</head>
 	<body>
 
--- include/register.php
+++ include/register.php
@@ -1,10 +1,27 @@
+<?php
+	
+	if( isset($_POST['register-user']) ){
+
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		var_dump($name);
+		var_dump($email);
+		var_dump($password);
+ 
+	}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>Register</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" type="image/png" href="/../assets/images/favicon.jpg">
 		<link rel="stylesheet" type="text/css" href="/../assets/css/app.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	</head>
 	<body>
 
@@ -21,24 +38,26 @@
 				<div id="name-container">
 					<label for="name">Name:</label>
 					<input type="text" name="name" id="name" autocomplete="off" />
-					<span id="name-error-container">test</span>
+					<span id="name-error-container"></span>
 				</div>
 
 				<div id="email-container">
 					<label for="email">Email:</label>
 					<input type="text" name="email" id="email" autocomplete="off" />
-					<span id="email-error-container">test</span>
+					<span id="email-error-container"></span>
 				</div>
 
 				<div id="password-container">
 					<label for="password">Password:</label>
 					<input type="password" name="password" id="password" />
-					<span id="password-error-container">test</span>
+					<span id="password-error-container"></span>
 				</div>
 
 				<div id="register-button-container">
 					<button type="button" id="register-button">REGISTER</button>
 				</div>
+
+				<input type="hidden" name="register-user" value="register-user" />
 
 			</form>
 		</div>
